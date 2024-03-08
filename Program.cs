fb340d769882ed6d0173c4a2bd84ee4b5e5a62e4
mcdevittp <mcdevittp1@mymail.nku.edu>
2024-03-08 12:14:17 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DiscountDelegateDemoPMM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
