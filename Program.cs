817b02b3a6a3c70d3c7d744343a35918858abad2
mcdevittp <mcdevittp1@mymail.nku.edu>
2024-03-08 12:31:20 -0500

    completed part 1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,44 @@
-﻿namespace DiscountDelegateDemoPMM
+﻿using System;
+using static System.Console;
+delegate void DiscountHandler(ref double saleAmount);
+namespace DiscountDelegateDemoPMM
 {
-    internal class Program
+    class DiscountDelegateDemo
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            double saleAmount;
+            char code;
+            DiscountHandler firstDel, secondDel;
+            firstDel = new DiscountHandler(StandardDiscount);
+            secondDel = new DiscountHandler(PreferredDiscount);
+            Write("Enter amount of sale ");
+            saleAmount = Convert.ToDouble(Console.ReadLine());
+            Write("Enter S for standard discount," + "or P for preferred discount ");
+            code = Convert.ToChar(ReadLine());
+            if (code == 'S')
+                firstDel(ref saleAmount);
+            else
+                secondDel(ref saleAmount);
+            WriteLine("New sale amount is {0}", saleAmount.ToString("C2"));
         }
+        public static void StandardDiscount(ref double saleAmount)
+        {
+            const double DISCOUNT_RATE = 0.05;
+            const double CUTOFF = 1000.00;
+            double discount;
+            if(saleAmount >= CUTOFF)
+                discount = saleAmount * DISCOUNT_RATE;
+            else
+                discount = 0;
+            saleAmount -= discount;
+        }
+        public static void PreferredDiscount(ref double saleAmount) 
+        {
+            const double SPECIAL_DISCOUNT = 0.10;
+            double discount = saleAmount * SPECIAL_DISCOUNT;
+            saleAmount -= discount;
+        }
+
     }
 }
